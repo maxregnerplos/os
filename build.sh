79aa2fce14a2c27da11d16fb197044529e24977c
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2023-04-15 21:32:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -9,42 +9,49 @@ if [[ "$(id -u)" != 0 ]]; then
 fi
 
 # get config
-if [ -n "$1" ]; then
-  CONFIG_FILE="$1"
-else
-  CONFIG_FILE="etc/terraform.conf"
-fi
+CONFIG_FILE="${1:-etc/terraform.conf}"
 BASE_DIR="$PWD"
-source "$BASE_DIR"/"$CONFIG_FILE"
+source "${BASE_DIR}/${CONFIG_FILE}"
 
-echo -e "
-#----------------------#
-# INSTALL VANILLA REPO #
-#----------------------#
-"
+echo -e "----------------------"
+echo -e "INSTALL LINUX REPO"
+echo -e "----------------------"
+
+# Add base linux sources
+cat > /etc/apt/sources.list.d/base-linux.list <<EOF
+deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy main
+EOF
+
+# Add base linux repo keys
+curl -sSL https://repo.baselinux.org/KEY.gpg | apt-key add -
+cp "${BASE_DIR}/etc/config/includes.chroot/usr/share/keyrings/base_linux_keyring.gpg" /usr/share/keyrings/
+
+# Remove stock debian sources
+rm -f /etc/apt/sources.list.d/debian.sources.list
+
+echo -e "----------------------"
+echo -e "INSTALL VANILLA REPO"
+echo -e "----------------------"
 
 # Add vanilla sources
 cat > /etc/apt/sources.list.d/vanilla-base.list <<EOF
 deb [arch=amd64] http://repo.vanillaos.org/ $BASECODENAME main
 EOF
 
-# Add vanilla repo key
-apt-key add "$BASE_DIR"/etc/config/archives/vanilla.key
-apt-key add "$BASE_DIR"/etc/config/archives/vanilla-main.key
-
-# Add vanilla keyring
-cp "$BASE_DIR"/etc/config/includes.chroot/usr/share/keyrings/vanilla_keyring.gpg /usr/share/keyrings/
+# Add vanilla repo keys
+curl -sSL https://repo.vanillaos.org/KEY.gpg | apt-key add -
+curl -sSL https://repo.vanillaos.org/MAIN-KEY.gpg | apt-key add -
+cp "${BASE_DIR}/etc/config/includes.chroot/usr/share/keyrings/vanilla_keyring.gpg" /usr/share/keyrings/
 
 # Remove stock debian sources
-rm -f /etc/apt/sources.list.d/debian.sources
+rm -f /etc/apt/sources.list.d/debian.sources.list
 
-echo -e "
-#----------------------#
-# INSTALL DEPENDENCIES #
-#----------------------#
-"
+echo -e "----------------------"
+echo -e "INSTALL DEPENDENCIES"
+echo -e "----------------------"
+
 apt-get update
-apt-get install -y live-build patch gnupg2 binutils zstd ca-certificates
+apt-get install -y live-build patch gnupg2 binutils lz4 ca-certificates
 dpkg -i debs/*.deb
 
 # TODO: workaround a bug in lb by increasing number of blocks for creating efi.img
@@ -55,62 +62,11 @@ ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/luna
 
 build () {
   BUILD_ARCH="$1"
-  mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
-  cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
+  mkdir -p "${BASE_DIR}/tmp/${BUILD_ARCH}"
+  cd "${BASE_DIR}/tmp/${BUILD_ARCH}" || exit
 
   # remove old configs and copy over new
   rm -rf config auto
-  cp -r "$BASE_DIR"/etc/* .
+  cp -r "${BASE_DIR}/etc/"* .
   # Make sure conffile specified as arg has correct name
-  cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
-
-  # Symlink chosen package lists to where live-build will find them
-  ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
-
-  echo -e "
-
-#------------------#
-# LIVE-BUILD CLEAN #
-#------------------#
-"
-  lb clean
-
-  echo -e "
-#-------------------#
-# LIVE-BUILD CONFIG #
-#-------------------#
-"
-  lb config
-
-  echo -e "
-#------------------#
-# LIVE-BUILD BUILD #
-#------------------#
-"
-  lb build
-
-  echo -e "
-#---------------------------#
-# MOVE OUTPUT TO BUILDS DIR #
-#---------------------------#
-"
-  YYYYMMDD="$(date +%Y%m%d)"
-  OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
-  mkdir -p "$OUTPUT_DIR"
-  FNAME="VanillaOS-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
-  mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
-
-  # cd into output to so {FNAME}.sha256.txt only
-  # includes the filename and not the path to
-  # our file.
-  cd $OUTPUT_DIR
-  md5sum "${FNAME}.iso" > "${FNAME}.md5.txt"
-  sha256sum "${FNAME}.iso" > "${FNAME}.sha256.txt"
-  cd $BASE_DIR
-}
-
-if [[ "$ARCH" == "all" ]]; then
-    build amd64
-else
-    build "$ARCH"
-fi
+  cp -f "${BASE_DIR}/${CONFIG_FILE}" terraform.conf
