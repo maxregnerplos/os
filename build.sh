7af7729aa1aa638edd5041bb1b73da8d1ddfa3e6
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2023-04-15 21:51:53 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -63,11 +63,61 @@ ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/luna
 
 build () {
   BUILD_ARCH="$1"
-  mkdir -p "${BASE_DIR}/tmp/${BUILD_ARCH}"
-  cd "${BASE_DIR}/tmp/${BUILD_ARCH}" || exit
+  mkdir -p "$BASE_DIR/tmp/$BUILD_ARCH"
+  cd "$BASE_DIR/tmp/$BUILD_ARCH" || exit
 
   # remove old configs and copy over new
   rm -rf config auto
-  cp -r "${BASE_DIR}/etc/"* .
+  cp -r "$BASE_DIR"/etc/* .
   # Make sure conffile specified as arg has correct name
-  cp -f "${BASE_DIR}/${CONFIG_FILE}" terraform.conf
+  cp -f "$BASE_DIR"/"$CONFIG_FILE" terraform.conf
+
+  # Symlink chosen package lists to where live-build will find them
+  ln -s "package-lists.$PACKAGE_LISTS_SUFFIX" "config/package-lists"
+
+  echo -e "
+#------------------#
+# LIVE-BUILD CLEAN #
+#------------------#
+"
+  lb clean
+
+  echo -e "
+#-------------------#
+# LIVE-BUILD CONFIG #
+#-------------------#
+"
+  lb config
+
+  echo -e "
+#------------------#
+# LIVE-BUILD BUILD #
+#------------------#
+"
+  lb build
+
+  echo -e "
+#---------------------------#
+# MOVE OUTPUT TO BUILDS DIR #
+#---------------------------#
+"
+  YYYYMMDD="$(date +%Y%m%d)"
+  OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
+  mkdir -p "$OUTPUT_DIR"
+  FNAME="VanillaOS-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
+  mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
+
+  # cd into output to so {FNAME}.sha256.txt only
+  # includes the filename and not the path to
+  # our file.
+  cd $OUTPUT_DIR
+  md5sum "${FNAME}.iso" > "${FNAME}.md5.txt"
+  sha256sum "${FNAME}.iso" > "${FNAME}.sha256.txt"
+  cd $BASE_DIR
+}
+
+if [[ "$ARCH" == "all" ]]; then
+    build amd64
+else
+    build "$ARCH"
+    fi
